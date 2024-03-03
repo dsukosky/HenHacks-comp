391ce26cb767eb9b460e4a85a6369304f81a2ad8
Dylan <dsukosky@udel.edu>
2024-03-02 19:36:20 -0500

    More Progress Made

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
 import pygame
-import random
+from random import random
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -14,8 +14,18 @@ boosting = False
 boost_timeout = False
 boost_color = "white"
 
-pointx = 70
-pointy = 200
+pointx = 0
+pointy = 0
+
+oposx = 61
+oposy = 81
+osize = 10
+ospeedx = 100
+ospeedy = 100
+ocolor = pygame.Color(60, 163, 23, 255)
+
+score = 0
+changing = True
 
 while running:
     # poll for events
@@ -44,6 +54,37 @@ while running:
 
     pygame.draw.circle(screen, "yellow", (pointx, pointy), 5)
 
+    pygame.draw.circle(screen, ocolor, (oposx, oposy), osize)
+    if 60 < oposx < 1240 and 80 < oposy < 690:
+        oposx += ospeedx * dt
+        oposy += ospeedy * dt
+
+    if changing:
+        pointx = 100 + (random() * 1100)
+        pointy = 130 + (random() * 520)
+        changing = False
+
+    if player_pos.x > pointx - 20  and player_pos.x < pointx + 20:
+        if player_pos.y > pointy - 20 and player_pos.y < pointy + 20:
+            score += 1
+            changing = True
+
+    if score >= 10:
+        ocolor = pygame.Color(163, 163, 23)
+        oposx += ospeedx * dt
+        oposy += ospeedy * dt
+    if score >= 20:
+        ocolor = pygame.Color(163, 114, 23)
+    if score >= 30:
+        ocolor = pygame.Color(163, 39, 23)
+    if score >= 40:
+        ocolor = pygame.Color(159, 23, 163)
+    if score >= 50:
+        ocolor = pygame.Color(51, 23, 163)
+    if score >= 100:
+        ocolor = pygame.Color(31, 237, 230)
+
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w] and player_pos.y > 85:
         player_pos.y -= 300 * dt
@@ -55,15 +96,15 @@ while running:
         player_pos.x += 300 * dt
     if keys[pygame.K_SPACE] and boost > 0 and boost_timeout == False:
         boosting = True
-        boost -= 1
+        boost -= 4
         if keys[pygame.K_w] and player_pos.y > 85:
-            player_pos.y -= 600 * dt
+            player_pos.y -= 450 * dt
         if keys[pygame.K_s] and player_pos.y < 688:
-            player_pos.y += 600 * dt
+            player_pos.y += 450 * dt
         if keys[pygame.K_a] and player_pos.x > 64:
-            player_pos.x -= 600 * dt
+            player_pos.x -= 450 * dt
         if keys[pygame.K_d] and player_pos.x < 1238:
-            player_pos.x += 600 * dt 
+            player_pos.x += 450 * dt 
 
     if not boosting and boost < 100:
         boost += 1
